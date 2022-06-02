dc23587275e309726b55e481ff27b2d12f0efa5d
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-02 14:45:54 -0700

    pycode style fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-        for l in range(len(str)):
-                print("{}".format(str[l] if ord(str[l]) < 97 or ord(str[l]) >
-                                  123 else chr(ord(str[l]) - 32)), end="")
-        print()
+    for i in range(len(str)):
+        print("{}".format(str[i] if ord(str[i]) < 97 or ord(str[i]) >
+            123 else chr(ord(str[i]) - 32)), end="")
+    print()
