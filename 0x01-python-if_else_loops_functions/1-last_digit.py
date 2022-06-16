8cc727754f16e0f77c05e38d32dd3252ae9a46dd
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-15 22:05:12 -0700

    pushing laast digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,11 @@ lno = abs(number) % 10
 if number < 0:
     lno *= -1
 if lno != 0 and lno > 5:
-    print("Last digit of ", number, "is", lno, "and is greater than 5")
+    print("Last digit of", number, "is", lno, "and is greater than 5")
 elif lno == 0:
-    print("Last digit of ", number, "is", lno, "and is 0")
+    print("Last digit of", number, "is", lno, "and is 0")
 elif lno != 0 and lno < 6:
-    print("Last digit of ", number, "is", lno, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", lno, "and is less than 6 and not 0")
 elif lno != 0 and lno < 0:
-    print("Last digit of ", number, "is", lno, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", lno, "and is less than 6 and not 0")
 # YOUR CODE HERE
