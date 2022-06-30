c35c65fb33fd1dd6e250ba8c7e5289ce6a6a3eab
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-30 20:11:37 +0000

    Pushing test development project

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""
-    Add two integers and return its sum
+"""Integer addition.
+
+This module contains only one function to add two integers.
+
+Example:
+    add_integer(5, 2) should give the result 7.
+
 """
 
 
 def add_integer(a, b=98):
-    """ Add two integers
-        Args:
-            a (int): first  integer
-            b (int): second integer
-        Return:
-            Sum of the two integers   
+    """Function that adds two integers.
+
+    Args:
+        a (int or float): First number.
+        b (int or float): Second number.
+
+    Returns:
+        int: The addition of a and b.
+
     """
-    if not isinstance(a, (int, float)):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return a + b
+    if a + 1 == a or b + 1 == b:
+        raise OverflowError("Number too large")
+    return int(a) + int(b)
