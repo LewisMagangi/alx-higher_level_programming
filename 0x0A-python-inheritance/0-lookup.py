c2bd1234d5e4efb2b5a558a0c87c96ab36e538d8
Liquelaliqour <lewismomanyi34@gmail.com>
2022-07-05 16:42:10 +0000

    pushing files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module contains just a function (lookup)
+"""
+def lookup(obj):
+    """
+    Function that returns the list of attributes and methods of an object
+    """
+    return dir(obj)
