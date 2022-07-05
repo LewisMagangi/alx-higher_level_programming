8e6d813a22eb28b88028ce7cb2602866b4844404
Liquelaliqour <lewismomanyi34@gmail.com>
2022-07-05 17:47:42 +0000

    more codes

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module contains the function class_to_json.
+"""
+def class_to_json(obj):
+    """
+    Function that returns dictionary description.
+    """
+    return obj.__dict__
