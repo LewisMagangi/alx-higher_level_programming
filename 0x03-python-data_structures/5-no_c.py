5e653d553501214a4ce924dcf21e28f96466a241
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-12 07:39:06 -0800

    final check

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     x = list(my_string)
     count = 0
     for i in x:
-        if i == "c" or i == "C":
+        if i == 'c' or i == 'C':
             x[count] = ""
         count += 1
 
