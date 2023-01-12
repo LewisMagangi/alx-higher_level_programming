5a85209bf4d3c52858dad8be3b65dd8161023eb6
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-12 05:26:13 -0800

    replacing characters in a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def no_c(my_string):
-    my_string_list = list(my_string)
-    index_count = 0
-    for index in my_string_list:
-        if index == 'c' or index == 'C':
-            my_string_list[index_count] = ""
-        index_count += 1
-    return "".join(my_string_list)
+    x = list(my_string)
+    for i in range(0, len(x) - 1):
+        if x[i] == "c" or x[i] == "C":
+            x[i] = ""
+
+    return "".join(x)
