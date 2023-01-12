acea2c829057520c0cdf6471def9c555bfe37ab5
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-12 07:33:36 -0800

    style check 2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3#
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
-    else:
-        for i in matrix:
-            count = 0
-            for x in i:
-                count += 1
-                print("{:d}".format(x), end=" " if len(i) > count else "")
-            print("\n")
+    for i in matrix:
+        count = 0
+        for x in i:
+            count += 1
+            print("{:d}".format(x), end=" " if len(i) > count else "")
+        print()
+        
