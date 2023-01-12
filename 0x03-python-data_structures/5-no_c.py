60f6ba6b150795bcba237aa076330185ec6b6f90
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-12 05:57:55 -0800

    amazing

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     x = list(my_string)
-    for i in range(0, len(x) - 1):
-        if x[i] == "c" or x[i] == "C":
-            x[i] = ""
+    count = 0
+    for i in x:
+        if i == "c" or i == "C":
+            x[count] = ""
+        count += 1
 
-    return("".join(x))
+    return "".join(x)
