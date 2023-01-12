eb19ba6b182bd479327896452e325648153bf539
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-12 01:47:17 -0800

    just wow

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        print("{}".format(i))
+        if i is int:
+            print("{}".format(i))
+        else:
+            raise Exception
