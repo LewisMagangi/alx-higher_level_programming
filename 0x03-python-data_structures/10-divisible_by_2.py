a5d152e26daad2739800c426d1a8a86aa27c87bf
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-13 04:31:49 -0800

    Return a new list with True or False, depending on whether the integer at the same position in the original list is a multiple of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = []
-    for elem in my_list:
-        result.append(True if not elem % 2 else False)
-    return result
+    newlist = []
+    for i in my_list:
+        if i % 2 == 0:
+            newlist.append(True)
+        else:
+            newlist.append(False)
+    return newlist
