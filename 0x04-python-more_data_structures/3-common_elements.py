23c9e7b06e8109f12c6f3a805a8d72835bb53cdb
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-13 07:27:16 -0800

    common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if not set_1 or not set_2:
-        return []
-    return set_1 & set_2
+    for i in set_1:
+        if i in set_2:
+            return i
