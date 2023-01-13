53171ff4df4351f8aec39d40fdd231b380129d53
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-13 07:45:05 -0800

    quick fix

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        if i in set_2:
-            return i
+    x = []
+    for i in list(set_1):
+        if i in list(set_2):
+            x.append(i)
+    return set(x)
--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if set_1 is None or set_2 is None:
-        return []
-    return set_1 ^ set_2
+    newlist = []
+    secondlist = []
+    for i in set_1:
+        if i not in set_2:
+            newlist.append(i)
+
+    for x in set_1:
+        if x not in set_1:
+            secondlist.append(x)
+    return newlist.append()
