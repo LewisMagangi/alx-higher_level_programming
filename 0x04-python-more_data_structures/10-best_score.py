4537dba77dfffe09e783a5f3ed746566c05b365b
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-18 06:31:57 -0800

    finding the best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary or len(a_dictionary) == 0:
+    if a_dictionary:
+        b = max(sorted(a_dictionary.values()))
+        return list(a_dictionary.keys())[list(a_dictionary.values()).index(b)]
+    else:
         return None
-    return max(a_dictionary, key=a_dictionary.get)
