54937deb0da145099c8564ec980cbc6861c39060
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-18 05:59:43 -0800

    multiplying by two

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if not a_dictionary:
-        return None
-    temp = dict(a_dictionary)
-    for key, value in temp.items():
-        temp[key] = value * 2
-        return temp
+    n = {}
+    for i in a_dictionary:
+        n[i] = 2 * a_dictionary.get(i)
+    return n
