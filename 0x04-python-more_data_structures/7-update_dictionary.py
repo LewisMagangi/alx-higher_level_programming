591d395eca11e5e11ab5043185b5a4b6ba6f7473
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-18 05:29:07 -0800

    updating dictionaries

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for i in range(len(a_dictionary)):
+    for i in range(len(a_dictionary) + 1):
         a_dictionary[key] = value
     return a_dictionary
