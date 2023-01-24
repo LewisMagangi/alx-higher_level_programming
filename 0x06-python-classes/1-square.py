80b4de5a376be8d18fe183535e0374648003d7d9
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-24 04:21:20 -0800

    private instance variable

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a class that defines a square
+"""
+
+
+class Square:
+     def __init__(self, size):
+         self.size = size
+    """
+    an empyt class
+    """
+    pass
