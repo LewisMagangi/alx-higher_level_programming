3e612d04a56d9613c262127bb303d4486dddc98b
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-24 04:30:54 -0800

    quick comment fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,9 +5,8 @@ a class that defines a square
 
 
 class Square:
-    def __init__(self, size):
-        self.__size = size
     """
     an empyt class
     """
-    pass
+    def __init__(self, size):
+        self.__size = size
