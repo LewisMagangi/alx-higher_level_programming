c5e82ded96a423bf3510bfefb9a83315b39e1825
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-27 03:15:20 -0800

    finally

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,9 +14,9 @@ class Rectangle:
         elif int(height) < 0:
             raise ValueError("height must be >= 0")
         elif type(width) is not int:
-            raise TypeError("weight must be an integer")
+            raise TypeError("width must be an integer")
         elif int(width) < 0:
-            raise ValueError("weight must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = width
             self.__height = height
