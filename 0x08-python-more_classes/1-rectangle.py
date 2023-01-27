7b641c9dfc89ee4d54097653061b55fd53359c05
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-27 02:59:07 -0800

    just wow

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,8 +17,9 @@ class Rectangle:
             raise TypeError("weight must be an integer")
         if int(width) < 0:
             raise ValueError("weight must be >= 0")
-        self.__width = width
-        self.__height = height
+        else:
+            self.__width = width
+            self.__height = height
     """
     a private instance method that retrieves the width of a rectangle
     """
