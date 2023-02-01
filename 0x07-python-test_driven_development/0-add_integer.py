65c55595fcd57fa3c8cc22e5eb4d89a491890c6d
Liquelaliqour <lewismomanyi34@gmail.com>
2023-02-01 07:54:22 -0800

    test driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,7 +2,13 @@
 """
 a function that adds two intergers
 """
+
+
 def add_integer(a, b=98):
-    if type(a) == float or type(b) == float:
-        a , b = int(a), int(b)
+    if type(a) != float and type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != float and type(b) != int:
+        raise TypeError("b must be an integer")
+    elif type(a) == float or type(b) == float:
+        a, b = int(a), int(b)
     return a + b
