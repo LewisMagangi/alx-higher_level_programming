d38ed7908878e333fe9270080ce1775cd5ada633
Liquelaliqour <lewismomanyi34@gmail.com>
2023-02-01 10:06:32 -0800

    indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python3
-"""Text indentation.
-
-This module contains just one function that indents text.
-
-Example:
-    text_indentation("Hello. I'm Darwin")
-    Should return:
-    Hello.
-    I'm Darwin
-"""
-
-
+'''
+Write a function that prints a text with 2 new lines after each of these characters: ., ? and :
+'''
 def text_indentation(text):
-    """Function that separates a text when a . ? or : is found.
+    '''
+    a function that prints a text with 2 new lines after each of these characters: ., ? and :
+    '''
 
-    Args:
-        text (str): Text to be formatted.
-    """
-    newline = 1
-    spaces = 0
-    if type(text) is not str:
-        raise TypeError("text must be a string")
-    for char in text:
-        if char == ' ':
-            spaces += 1
-        elif char == '.' or char == '?' or char == ':':
-            if not newline:
-                print(" " * spaces, end="")
-            print(char + "\n")
-            newline = 1
-        elif not newline:
-            print(" " * spaces + char, end="")
-            spaces = 0
+    if type(text) != str:
+        raise TypeError('text must be a string')
+    for i in text:
+        if i in ['.', '?', ':']:
+            print('\n')
         else:
-            print(char, end="")
-            newline = 0
-            spaces = 0
+            print(i, end='')
