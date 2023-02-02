d64aef3030638d3a7d08bbc022c0ce4788f70339
Liquelaliqour <lewismomanyi34@gmail.com>
2023-02-02 05:43:44 -0800

    fixing some last hidden edge case doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,10 +8,12 @@ def add_integer(a, b=98):
     '''
     Returns an integer: the addition of a and b
     '''
-    if type(a) != float and type(a) != int:
+    x = [float, int]
+    ta , tb = type(a), type(b) 
+    if ta not in x:
         raise TypeError("a must be an integer")
-    elif type(b) != float and type(b) != int:
+    elif tb not in x:
         raise TypeError("b must be an integer")
-    elif type(a) == float or type(b) == float:
+    elif ta == x[0] or tb == x[0]:
         a, b = int(a), int(b)
     return a + b
