56f490f407a5f85112b12cd625e40c8e8013c63a
Liquelaliqour <lewismomanyi34@gmail.com>
2023-02-02 01:03:20 -0800

    list comprehension makes a deep copy unlike slicing and using the inbuilt copy

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,22 +6,23 @@ def matrix_divided(matrix, div):
     '''
     a function that divides all elements of a matrix.
     '''
-    new_matrix = matrix[:]
-    if type(matrix) != list:
+    new_matrix = [[u for u in f] for f in matrix]
+    if type(new_matrix) != list:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    list_len = len(matrix[0])
-    for i in range(len(matrix)):
-        if type(matrix[i]) is not list:
+    list_len = len(new_matrix[0])
+    for i in range(len(new_matrix)):
+        if type(new_matrix[i]) is not list:
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        elif len(matrix[i]) != list_len:
+        elif len(new_matrix[i]) != list_len:
             raise TypeError("Each row of the matrix must have the same size")
-        for x in range(len(matrix[i])):
-            if type(matrix[i][x]) != int and type(matrix[i][x]) != float:
+        for x in range(len(new_matrix[i])):
+            if type(new_matrix[i][x]) != int and type(new_matrix[i][x]) != float:
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             new_matrix[i][x] /= div
-    
+            new_matrix[i][x] = round(new_matrix[i][x], 2)
+
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     elif div == 0:
         raise ZeroDivisionError('division by zero')
-    return matrix
+    return new_matrix
