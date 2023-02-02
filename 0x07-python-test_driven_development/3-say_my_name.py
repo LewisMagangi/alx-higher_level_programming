a803463b69cf1aba2b88d04f777b5f704eae87c4
Liquelaliqour <lewismomanyi34@gmail.com>
2023-02-02 07:34:44 -0800

    say my name doctest fix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,12 +3,13 @@
 Write a function that prints My name is <first name> <last name>
 '''
 
+
 def say_my_name(first_name, last_name=""):
     '''
     a function that prints My name is <first name> <last name>
     '''
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    elif not isinstance(last_name, str):
+    elif type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print(f'My name is {first_name}{last_name}')
