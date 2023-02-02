18cadd83c1996d77bfd5c2d5cd91cf0eb6a02207
Liquelaliqour <lewismomanyi34@gmail.com>
2023-02-02 06:59:30 -0800

    minor pycodestyle documentation change

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,28 +8,22 @@ def matrix_divided(matrix, div):
     '''
     a function that divides all elements of a matrix.
     '''
+    s = "matrix must be a matrix (list of lists) of integers/floats"
     new = [[u for u in f] for f in matrix]
     if type(div) not in [int, float]:
-        raise TypeError("div must be a number")
+        raise TypeError('div must be a number')
     elif div == 0:
         raise ZeroDivisionError('division by zero')
     if type(new) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    list_len = len(new[0])
-    matrix_len = len(new)
+        raise TypeError(s)
     for i in range(len(new)):
         if type(new[i]) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        elif len(new[i]) not in [matrix_len, list_len]:
+            raise TypeError(s)
+        elif len(new[i]) not in [len(new), len(new[0])]:
             raise TypeError("Each row of the matrix must have the same size")
         for x in range(len(new[i])):
             if type(new[i][x]) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(s)
             new[i][x] /= div
             new[i][x] = round(new[i][x], 2)
-
-    if type(div) not in [int, float]:
-        raise TypeError("div must be a number")
-    elif div == 0:
-        raise ZeroDivisionError('division by zero')
     return new
