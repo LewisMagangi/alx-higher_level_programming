f56e4ef41578641f331a816c7322e3cf7431940f
Liquelaliqour <lewismomanyi34@gmail.com>
2023-07-19 01:00:42 -0700

    empty geometry class doc fix

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -5,4 +5,7 @@ An empty class
 
 
 class BaseGeometry:
+    """
+    Empty class.
+    """
     pass
--- 0x0A-python-inheritance/5-main.py
+++ 0x0A-python-inheritance/5-main.py
@@ -4,5 +4,3 @@ BaseGeometry = __import__('5-base_geometry').BaseGeometry
 bg = BaseGeometry()
 
 print(bg)
-print(dir(bg))
-print(dir(BaseGeometry))
