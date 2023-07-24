44fb2ff0b41602846e6c643e062e67d39a0b6d89
Liquelaliqour <lewismomanyi34@gmail.com>
2023-07-24 03:17:29 -0700

    json serializable vs non - serializable

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,8 +2,12 @@
 """
 This module contains the function class_to_json.
 """
+
+
 def class_to_json(obj):
     """
-    Function that returns dictionary description.
+    a function that returns the dictionary description
+    with simple data structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object:
     """
     return obj.__dict__
