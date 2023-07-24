ec93fd18d34559db0aac22eeda44b6aeda8c5213
Liquelaliqour <lewismomanyi34@gmail.com>
2023-07-24 03:44:04 -0700

    method def to_json(self): that retrieves a dictionary representation of a Student instance

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """
-This module contains the class Student.
+A class that defines a student
 """
+
+
 class Student:
     """
-    Class that defines a student.
+    a public instance attribute containing first_name, last_name and age
     """
-
     def __init__(self, first_name, last_name, age):
-        """
-        Initializer for the new Student objects.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
+    """
+    a public instance attribute that retrieves
+    a dictionary representation of a Student instance
+    """
     def to_json(self):
-        """
-        Returns the dictionary representation of a class.
-        """
-        return self.__dict__
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+                    }
