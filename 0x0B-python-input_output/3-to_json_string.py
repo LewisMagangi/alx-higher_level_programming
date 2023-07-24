77f14f3fc0d388e64c02ba1fd3d387b9d8e3d650
Liquelaliqour <lewismomanyi34@gmail.com>
2023-07-24 00:18:55 -0700

    json rep return

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-This module contains the function to_json_string.
+File Input Output Module
 """
 import json
 
+
 def to_json_string(my_obj):
     """
-    Function that returns the json representation of an object.
+    a function that returns the JSON representation of an object (string):
     """
     return json.dumps(my_obj)
