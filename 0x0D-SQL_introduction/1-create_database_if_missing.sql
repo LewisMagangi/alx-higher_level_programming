636ddbb9defad9fd0126e93606cd2e79aaf48f7d
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-03 06:44:07 +0000

    trying another alternative

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,11 @@
 -- a script that creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE hbtn_0c_0;
+IF DB_ID('hbtn_0c_0') IS NOT NULL
+BEGIN
+    -- Code to execute if the condition is true	
+    CREATE DATABASE hbtn_0c_0;
+END
+ELSE
+BEGIN
+    -- Code to execute if the condition is false
+    CREATE DATABASE hbtn_0c_0;
+END
