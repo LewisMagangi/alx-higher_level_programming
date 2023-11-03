f928786b3e120a3bf3cbaaa58d0abe6b89df280a
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-03 06:59:46 +0000

    BACKTICKS check

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,11 +1,11 @@
 -- a script that creates the database hbtn_0c_0 in your MySQ
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'YourDatabaseName')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = `hbtn_0c_0`)
 BEGIN
     -- Database exists
-    CREATE DATABASE hbtn_0c_0;
+    CREATE DATABASE `hbtn_0c_0`;
 END;
 ELSE
 BEGIN
     -- Database does not exist
-    CREATE DATABASE hbtn_0c_0;
+    CREATE DATABASE `hbtn_0c_0`;
 END;
