88c6ae17217a6d260ab4ff43fb160a85aa7b4de7
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-03 11:31:08 +0000

    quick fix only the score and the name should be listed

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT * FROM `second_table`
+SELECT `score`, `name`
+FROM `second_table`
 IF `name` EXISTS
 ORDER BY score DESC;
