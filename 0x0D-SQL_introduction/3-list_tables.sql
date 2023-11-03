c535e10759a2385308a04a85c1edf651dc1261e4
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-03 07:11:16 +0000

    lisitng all databases i my sql server

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,4 @@
--- Lists all the tables of my database in my MySQL server
-SHOW TABLES;
+-- a script that that lists all the tables of a database in your MySQL server.
+SELECT name
+FROM sys.databases;
+
