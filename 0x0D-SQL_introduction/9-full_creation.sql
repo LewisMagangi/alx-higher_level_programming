b2df589e1703e728a8b4fb4a558eb8b586b65624
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-03 10:09:05 +0000

    quick fix on 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,9 +5,9 @@ CREATE TABLE IF NOT EXISTS `second_table`(
        `score` INT
 );
 
--- Insert multiple records into the `second_table` table
+-- Insert multiple records into the second_table
 INSERT INTO `second_table` (id, name, score)
-VALUES (1, 'Bob',14),
-       (2, 'John', 10),
-       (3, 'George', 8),
-       (4, 'Alex', 3);
+VALUES (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
