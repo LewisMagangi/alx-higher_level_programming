244a7e55e894e5993b95b221172e179f48b0e816
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-07 03:31:38 -0800

    quick change

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This is  a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-echo "$(curl -i -X OPTIONS "$1" -s)"
+echo "$(curl -i -X OPTIONS "$1" -s  | grep -i "Allow" | awk -F ": " '{print $2}')
