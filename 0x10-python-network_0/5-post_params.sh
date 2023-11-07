6ab0d4db593480f527525971fb8b98a6ff8bc2e8
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-07 06:38:52 -0800

    syntax fix on the post script

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This is a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-echo "$(curl -X POST -d "$email=test@gmail.com&subject=subjectI will always be here for PLD" "$1")"
+echo "$(curl -X POST -d 'ee+wialways+be+here+for+PLD' "$1")"
