86258acfb2908d06cdfd76b27525dc7f3b2b26ed
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-07 02:03:22 -0800

    syntacx error fixed pushing the change to find out if the script works as intended

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This is a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-echo -n (curl -s "$0" | wc -c
+echo -n (curl -s "$0" | wc -c)
