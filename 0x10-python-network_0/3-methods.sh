e254b33bc5e69f13f6b0a860863a8d0b4a5e9153
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-07 03:09:29 -0800

    logical error in the methods script

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This is  a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-echo "$(curl -i -X OPTIONS "$url" -s)"
+echo "$(curl -i -X OPTIONS "$1" -s)"
