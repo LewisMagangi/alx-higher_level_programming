80133e6c21338df6cf8f05d875233c5e9ee51741
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-07 01:53:49 -0800

    the script should be exactly 3 lines commenting the other lines to check if the 3 newlin eof the intended script is correct

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#
+#This is a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
+echo -n (curl -s "$0" | wc -c
 : '
   This is a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
 
   The size must be displayed in bytes.
   You have to use curl.
-'
+
 # Check if an argument (URL) is provided
 if [ -z "$1" ]; then
     echo "Usage: $0 <URL>"
@@ -21,3 +22,4 @@ response=$(curl -s "$url")
 # Display the size of the response body in bytes
 response_size=$(echo -n "$response" | wc -c)
 echo $response_size
+'
