9012a79397642f871198b156b7052201d7fd860f
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-07 03:05:00 -0800

    fixing a syntax error on the script that sends a DELETE request

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This is a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-echo -n "$(curl -X "$1")"
+echo -n "$(curl -X DELETE -s "$1")"
