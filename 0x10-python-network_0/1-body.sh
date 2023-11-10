501e977baeceb50bdeda2c69a07c4bb3c4379d90
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-10 04:21:45 -0800

    Another approach

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #This is a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-#echo -n " $curl -s -X GET "$1")"
-curl -s -X GET "$1"
+if [ "$(curl -sLI "$1" -X GET | awk '/HTTP/ {print $2}')" = '200' ]; then curl -sL "$1"; fi
