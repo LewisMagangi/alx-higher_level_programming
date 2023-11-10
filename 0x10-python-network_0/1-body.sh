eaab5242e3e85838f5a7946aae7af8edf99d03b0
Liquelaliqour <lewismomanyi34@gmail.com>
2023-11-10 03:42:55 -0800

    another trial

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This is a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-[[ $(curl -s -w "%{http_code}" -o temp.txt "$1" && tail -n 1 temp.txt) -eq 200 ]] && cat temp.txt | head -n -1
+response=$(curl -s -w "%{http_code}" -o temp.txt "$1") && [[ $(tail -n 1 temp.txt) -eq 200 ]] && cat temp.txt | head -n -1; rm temp.txt
