23550e77b94f562abb5b2e11e1cfd31a9f686408
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-05 10:40:49 +0000

    new file to coujnt lines of code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const intVal = parseInt(process.argv[2]);
-if (!intVal) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + intVal);
+if (typeof(process.agrv[2]) === 'number' || typeof(process.agrv[2]) === 'string')
+{
+  console.log('My number: ' + parseInt(process.agrv[2]) || 'Not a number')
+} else 
+{
+  console.log('Not a number')
 }
