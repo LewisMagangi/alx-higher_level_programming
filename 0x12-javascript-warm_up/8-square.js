4713f8f2192b948aa9579f55370d08222bf03c9a
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-06 09:02:03 +0000

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
-let i = 0;
-if (!size) {
-  console.log('Missing size');
-} else {
-  for (i; i < size; i++) {
-    console.log('X'.repeat(size));
+const x = process.argv[2];
+const y = typeof x;
+if (((y === 'number' || y === 'string') && !isNaN(x))) {
+  for (let i = 0; i < parseInt(x); i++) {
+    let row = '';
+    for (let l = 0; l < parseInt(x); l++) {
+      row += 'X';
+    }
+    console.log(row);
   }
+} else {
+  console.log('Missing size');
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const n1 = parseInt(process.argv[2]);
-const n2 = parseInt(process.argv[3]);
-function add (a, b) {
-  return (a + b);
-}
-console.log(add(n1, n2));
+console.log(process.argv[2] + ' is ' + process.argv[3]);
