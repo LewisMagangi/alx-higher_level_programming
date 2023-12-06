89092462c524b65d56f8d81dd3a795a4b58a3407
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-06 09:24:03 +0000

    a script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+function add (a, b) {
+  console.log(Number(a) + Number(b));
+}
+// Get command line arguments
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Call the add function with command line arguments
+add(arg1, arg2);
