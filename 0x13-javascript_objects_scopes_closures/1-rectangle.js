287bdb4a801f0a43b8f9569858d30c4dbd15887f
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-08 15:25:50 +0000

    Fixing the style of the code to be semistandard compliant

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -4,9 +4,9 @@ class Rectangle {
   width;
   height;
 
-  constructor(width, height) {
-  this.width = width;
-  this.height = height
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
   }
 }
 module.exports = Rectangle;
