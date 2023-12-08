ca7700da9080f35f7af6f77319087337dd34dc04
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-08 15:15:01 +0000

    Initialize the instance attribute width with the value of w

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-  // This is an empty rectangle class
-}
+  // This is a class Rectangle that defines a rectangle:
+  width;
+  height;
 
+  constructor(width, height) {
+  this.width = width;
+  this.height = height
+  }
+}
 module.exports = Rectangle;
