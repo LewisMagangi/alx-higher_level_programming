47c6bc9fe8b750e18482dcdeff09303a53cbb30f
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-09 19:49:10 +0000

    I hope i found a way around

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,8 @@ class Rectangle {
   // This is a class Rectangle that defines a rectangle:
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
-      const emptyObject = {};
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+      const emptyRectangle = new Rectangle();
     } else {
       this.width = w;
       this.height = h;
