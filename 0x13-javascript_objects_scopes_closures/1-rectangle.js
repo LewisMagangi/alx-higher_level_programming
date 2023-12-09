9b52a89c59983b31a2c010c2dc721ac7e4babd25
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-09 17:20:13 +0000

    Naming convection change

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -4,9 +4,9 @@ class Rectangle {
   width;
   height;
 
-  constructor (width, height) {
-    this.width = width;
-    this.height = height;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
