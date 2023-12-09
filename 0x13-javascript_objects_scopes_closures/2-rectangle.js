76e0d5b3be66848e59a437d78e134a9623999ac3
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-09 20:02:30 +0000

    The condition if (w && h && w > 0 && h > 0) checks whether the variables w and h are truthy and whether both are greater than 0.

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,8 @@
 class Rectangle {
   // This is a class Rectangle that defines a rectangle:
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w && h && w > 0 && h > 0) {
       // If w or h is equal to 0 or not a positive integer, create an empty object
-      const emptyRectangle = new Rectangle();
-    } else {
       this.width = w;
       this.height = h;
     }
