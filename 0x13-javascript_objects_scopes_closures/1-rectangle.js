29a54844f9a0616abe647ccba9f03ba543e4e9e0
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-09 17:26:56 +0000

    Quick fix removing unnecesary tet.

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
   // This is a class Rectangle that defines a rectangle:
-  width;
-  height;
-
   constructor (w, h) {
     this.width = w;
     this.height = h;
