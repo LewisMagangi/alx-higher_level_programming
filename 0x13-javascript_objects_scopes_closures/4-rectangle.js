7cc4ee387a1daf9e93c406bb180b5240f01d4c88
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-10 15:04:12 +0000

    Implementation of rotate and double methods in the rectangle class

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,24 +1,59 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+
+class Rectangle {
+  // This is a class Rectangle that defines a rectangle:
   constructor (w, h) {
-    if (w && h && w > 0 && h > 0) {
+    // Check if w and h are positive integers
+    if (w > 0 && h > 0) {
+      // If valid, assign values to width and height
       this.width = w;
       this.height = h;
+    } else {
+      // If not valid, create an empty object
+      this.width = 0;
+      this.height = 0;
     }
   }
 
+  // Method to print a rectangle
   print () {
-    console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let l = 0; l < this.width; l++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
   }
 
+  // an instance method called rotate() that exchanges the width and the height of the rectangle
   rotate () {
-    this.width = this.width + this.height;
-    this.height = this.width - this.height;
-    this.width = this.width - this.height;
+    const x = this.height;
+    this.height = this.width;
+    this.width = x;
   }
 
+  // an instance method called double() that multiples the width and the height of the rectangle by 2
   double () {
-    this.width *= 2;
-    this.height *= 2;
+    this.height = this.height * 2;
+    this.width = this.width * 2;
   }
-};
+}
+// Specify width and height for the rectangle
+const w = 0;
+const h = 0;
+
+// Create an instance of Rectangle
+const myRectangle = new Rectangle(w, h);
+
+// Calling the instance method to print the rectangle
+myRectangle.print();
+
+// Calling the instance method to rotate the rectangle
+myRectangle.rotate();
+
+// Calling the instance method to double the rectangle
+myRectangle.double();
+
+// Export the Rectangle class
+module.exports = Rectangle;
