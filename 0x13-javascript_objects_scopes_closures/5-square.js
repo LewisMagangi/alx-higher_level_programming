9df2c59cd28eec42188d98a4bb1e9c6e2f1fdbdc
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-11 16:47:20 +0000

    quick fixex

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,15 +5,6 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size); // Pass size as both width and height to the Rectangle constructor
-
-    // Check if size is a positive integer
-    if (size > 0) {
-      // If valid, assign value to size
-      this.size = size;
-    } else {
-      // If not valid, create an empty
-      this.size = 0;
-    }
   }
 }
 
