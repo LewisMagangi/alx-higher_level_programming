13cc5e0bd8f06e1b53e6f54460cc243b4844a4af
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-11 18:15:00 +0000

    removing a trailing space to make the code semistandard compliant

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,7 +4,7 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super(size, size); // Pass size as both width and height to the Rectangle constructor 
+    super(size, size); // Pass size as both width and height to the Rectangle constructor
   }
 }
 
