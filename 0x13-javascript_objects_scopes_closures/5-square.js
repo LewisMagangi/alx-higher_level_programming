2003ea9872079c23685590e6cf51a81281c8a525
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-11 15:18:42 +0000

    a class Square that defines a square and inherits from Rectangle of 4-rectangle.js:

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,24 @@
 #!/usr/bin/node
+// Import the Rectangle class
 const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
+
+class Square extends Rectangle {
   constructor (size) {
-    super(size, size);
+    super(size, size); // Pass size as both width and height to the Rectangle constructor
+    this.size = size;
+
+    // Check if size is a positive integer
+    if (size > 0) {
+      // If valid, assign values to width and height
+      this.width = size;
+      this.height = size;
+    } else {
+      // If not valid, create an empty object
+      this.width = 0;
+      this.height = 0;
+    }
   }
-};
+}
+
+// Export the Square class
+module.exports = Square;
