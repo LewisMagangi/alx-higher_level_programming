b870afb742d2555cddc1be19f101d1c89070fada
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-11 18:23:14 +0000

    Different exporting styles to check if the functionality changes

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,11 +2,9 @@
 // Import the Rectangle class
 const Rectangle = require('./4-rectangle');
 
-class Square extends Rectangle {
+// Export the Square class
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size); // Pass size as both width and height to the Rectangle constructor
   }
 }
-
-// Export the Square class
-module.exports = Square;
