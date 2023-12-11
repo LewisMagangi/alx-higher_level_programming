749bb267ee502a5f5a3f2eddc6cfb0dfca0367ba
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-11 16:24:45 +0000

    quick fix on matters concerning the width and height of the square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,17 +5,14 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size); // Pass size as both width and height to the Rectangle constructor
-    this.size = size;
 
     // Check if size is a positive integer
     if (size > 0) {
-      // If valid, assign values to width and height
-      this.width = size;
-      this.height = size;
+      // If valid, assign value to size
+      this.size = size;
     } else {
-      // If not valid, create an empty object
-      this.width = 0;
-      this.height = 0;
+      // If not valid, create an empty
+      this.size = 0;
     }
   }
 }
