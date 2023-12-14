d097437823513ef041ceec2a69b465905e988a0b
Liquelaliqour <lewismomanyi34@gmail.com>
2023-12-14 10:15:08 -0800

    some style fix and permission given and some details downloaded to make the code funtional

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,15 +2,16 @@
 // Characters of a Star Wars movie
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
 request(url, 'utf8', (error, response, body) => {
   if (error) throw error;
   else {
-    for (const x of JSON.parse(body).characters) {
-      request(x, 'utf8', (error, response, body) => {
+    for (const characterUrl of JSON.parse(body).characters) {
+      request(characterUrl, 'utf8', (error, response, body) => {
         if (error) throw error;
-        else {
-          console.log(JSON.parse(body).name);
-        }
+	else {
+	  console.log(JSON.parse(body).name);
+	}
       });
     }
   }
