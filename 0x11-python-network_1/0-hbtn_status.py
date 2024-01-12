cc66031a6eb256e6509d46313786489461e82119
Liquelaliqour <lewismomanyi34@gmail.com>
2024-01-12 03:25:24 -0800

    Creating the desired output of the script.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,5 +5,9 @@ url = "https://alx-intranet.hbtn.io/status"
 request = urllib.request.Request(url)
 with urllib.request.urlopen(request) as response:
     html_content = response.read()
+    utf8_content = html_content.decode('utf-8')
 
-print(html_content)
+    print("Body response:")
+    print("\t- type:", type(html_content))
+    print("\t- content:", html_content)
+    print("\t- utf8 content:", utf8_content)
