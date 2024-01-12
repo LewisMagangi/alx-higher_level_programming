2f6a945c93bc6531916a8504a44f2356004ae074
Liquelaliqour <lewismomanyi34@gmail.com>
2024-01-12 05:09:30 -0800

    Documenting the module

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,13 @@
 import urllib.request
 import sys
 
+# Retrieve the URL from the command line argument
 url = sys.argv[1]
+
+# Create a Request object with the specified URL
 request = urllib.request.Request(url)
+
+# Use a with statement to open the URL and make the request
 with urllib.request.urlopen(request) as response:
     html_content = response.read()
     utf8_content = html_content.decode('utf-8')
