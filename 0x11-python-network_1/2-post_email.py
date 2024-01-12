ec0e4f611cea7d431366f61985aa56ba519002e9
Liquelaliqour <lewismomanyi34@gmail.com>
2024-01-12 06:38:24 -0800

    bllind push

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,9 +5,17 @@ sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)
 """
 import sys
-import urllib
-url = sys.argv[1]
-email = sys.argv[2]
+import urllib.parse
+import urllib.request
 
-response = requests.post(url, data={'email': email})
-print(response.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Encode the email parameter
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    # Make the POST request
+    with urllib.request.urlopen(url, email) as response:
+        print(response.read().decode('utf-8'))
+    
