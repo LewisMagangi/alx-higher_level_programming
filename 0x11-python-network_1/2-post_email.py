bdef076b93b89c39ad92227bc25ac6c1e8a80d9b
Liquelaliqour <lewismomanyi34@gmail.com>
2024-01-30 04:29:50 -0800

    PEP style compliant code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url, data=data) as response:
         print("Your email is: {}".format(email))
         print(response.read().decode('utf-8'))
-    
