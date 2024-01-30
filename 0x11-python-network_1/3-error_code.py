48951f65a29fbdfb64f7f0cadeeff1ec7caea8ba
Lewis <lewismomanyi34@gmail.com>
2024-01-30 17:31:58 +0300

    that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).A new script

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL, 
+sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+import sys
+import urllib.parse
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        html_bytes = response.read()
+        html = html_bytes.decode("utf-8")
+        print(html)
+
+except Exception as e:
+    print(f"Error code: {e.code}")
