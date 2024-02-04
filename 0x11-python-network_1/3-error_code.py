055151fe2fc80af1282c32f33dd3c0fa9b6ae17b
Liquelaliqour <lewismomanyi34@gmail.com>
2024-02-04 07:03:31 -0800

    Quick fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,14 +12,17 @@ import urllib.error
 if __name__ == "__main__":
     url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        html_bytes = response.read()
-        html = html_bytes.decode("utf-8")
-        print(html)
-
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
-
-except Exception as e:
-    print(f"Error code: {e.code}")
+    try:
+        with urllib.request.urlopen(url) as response:
+            html_bytes = response.read()
+            html = html_bytes.decode("utf-8")
+            print(html)
+            
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+        
+    except urllib.error.URLError as e:
+        # Handle URL-related errors
+        print(f"URLError: {e.reason}")
+    except Exception as e:
+        print(f"Error code: {e.code}")
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     
-    if response.status_code != 200:
+    if response.status_code == 200:
         print("Body response:")     
         print("\t- type:", type(response.text))
         print('\t- content:', response.text)
