6efca42fdfb97d7e59de2a70d609c5b121b0d4e3
Lewis <lewismomanyi34@gmail.com>
2024-02-04 22:52:03 +0300

    Additional Error Fix Implementation

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -13,5 +13,9 @@ if __name__ == "__main__":
     try:
         response = requests.get(url)
         print(response.text)
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Error code: {response.status_code}")
+    except requests.exceptions.RequestException as req_err:
+        print(f"Error code: {response.status_code}")
     except Exception as e:
         print(f"Error code: {response.status_code}")
