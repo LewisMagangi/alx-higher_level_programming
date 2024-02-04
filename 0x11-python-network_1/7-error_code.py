3a677fb3f1b487342bf4ea4b6e2bc25ea5ac43f3
Liquelaliqour <lewismomanyi34@gmail.com>
2024-02-04 12:53:32 -0800

    style fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,7 +10,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-        
+
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
