e7440228aeb228c6ea50d4fb6f6ac8f141522022
Liquelaliqour <lewismomanyi34@gmail.comm>
2024-05-08 16:40:23 +0300

    calculator importation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,11 @@
-print("checking conf of git if the intended commit is pushed to the intended user")
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
