b02e9914c605572af6ee9e81053c045d3faba743
Liquelaliqour <lewismomanyi34@gmail.comm>
2024-05-08 16:50:38 +0300

    a module runned as a script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"{i}: {arg}")
